809a39dac967855f735d3d14b401ee8cb9c7e1d2
miki <sableangle@gmail.com>
2019-06-14 16:16:17 +0800

    攝影機同步

---
+++ ProjectWatchVR_Unity/Assets/0_Games/Scritps/Level/AI_FrameLevel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_FrameLevel : MonoBehaviour
+{
+    Transform mainCameraTransform;
+    [SerializeField]
+    Transform oldRoomCamera;
+    void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    bool isPlayerIn = false;
+    [SerializeField]
+    Transform oldRoomOriLocalPosition;
+    [SerializeField]
+    Transform frameOriTransform;
+    void Update()
+    {
+        if (!isPlayerIn)
+        {
+            return;
+        }
+
+        var offset = frameOriTransform.position - mainCameraTransform.position;
+        offset.y *= -1;
+        var localOffect = frameOriTransform.InverseTransformPoint(offset);
+        localOffect.y = offset.y;
+        oldRoomCamera.localPosition = Vector3.Lerp(oldRoomCamera.localPosition, localOffect, 8 * Time.deltaTime);
+        oldRoomCamera.rotation = Quaternion.Lerp(oldRoomCamera.rotation, mainCameraTransform.rotation, 8 * Time.deltaTime);
+
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("MainCamera"))
+        {
+            return;
+        }
+        isPlayerIn = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("MainCamera"))
+        {
+            return;
+        }
+        isPlayerIn = false;
+    }
+}
